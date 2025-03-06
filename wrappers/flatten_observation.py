07fc4595852231a771aa7f41c19889ef79a7c1e8
Alexander Cai <alexandercai@outlook.com>
2025-03-05 22:40:37 -0500

    update observation space for flatten wrapper

--- wrappers/flatten_observation.py
+++ wrappers/flatten_observation.py
@@ -1,16 +1,12 @@
+import math
 from typing import Annotated
 
+import dm_env.specs as specs
 import jax
 import jax.numpy as jnp
 from jaxtyping import Array, Key
 
-from wrappers.base import (
-    Environment,
-    TAction,
-    TEnvParams,
-    TEnvState,
-    TObs,
-)
+from wrappers.base import Environment, TAction, TEnvParams, TEnvState, TObs
 
 
 def _flatten(x: Array):
@@ -33,4 +29,22 @@ def flatten_observation_wrapper(
         timestep.obs = _flatten(timestep.obs)
         return timestep
 
-    return env.wrap(name="flatten_observation", reset=reset, step=step)
+    def observation_space(params: TEnvParams) -> specs.Array:
+        obs_spec = env.observation_space(params)
+        assert isinstance(obs_spec, specs.BoundedArray), (
+            f"Base environment observation space must be BoundedArray. Got {obs_spec}"
+        )
+        return specs.BoundedArray(
+            shape=(math.prod(obs_spec.shape),),
+            dtype=obs_spec.dtype,
+            minimum=obs_spec.minimum,
+            maximum=obs_spec.maximum,
+            name=obs_spec.name,
+        )
+
+    return env.wrap(
+        name="flatten_observation",
+        reset=reset,
+        step=step,
+        observation_space=observation_space,
+    )
